218eb82c0abd9e5c8c7bb68f72149a824f3c46f6
TheNavin55 <88062937+TheNavin55@users.noreply.github.com>
2021-12-14 18:23:04 -0300

    Postman

--- Proyecto API REST/proyectoFinal/src/main/java/com/informatorio/proyectoFinal/controller/VotoController.java
+++ Proyecto API REST/proyectoFinal/src/main/java/com/informatorio/proyectoFinal/controller/VotoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/votos")
+@RequestMapping(value = "/usuarios/votos")
 public class VotoController {
     private final VotoService votoService;
     @Autowired
